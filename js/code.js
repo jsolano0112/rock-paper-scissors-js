f72e1added811042e7e23752df90918eaa1a8178
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-07-10 16:42:46 -0500

    Updating

--- js/code.js
+++ js/code.js
@@ -6,12 +6,16 @@ const TIE = 0;
 const WIN = 1;
 const LOST = 2;
 
+var attempsC = 0;
+var attempsU = 0;
+
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
 const resultText = document.getElementById("result")
 const computerEmoji = document.getElementById("computer");
 const userEmoji = document.getElementById("user")
+const message = document.getElementById("message")
 
 rockBtn.addEventListener("click", () => {
     play(ROCK);
@@ -27,35 +31,42 @@ function play(userOption) {
     const computerOption = Math.floor(Math.random() * 3);
     const result = calcResult(userOption, computerOption);
 
-    if(userOption === ROCK){
-        userEmoji.innerHTML = "🥌";
-    }else if(userOption === PAPER){
-        userEmoji.innerHTML = "📋";
-    }else if(userOption === SCISSORS){
-        userEmoji.innerHTML = "✂";
-    }
 
-    if(computerOption === ROCK){
-        computerEmoji.innerHTML = "🥌";
-    }else if(computerOption === PAPER){
-        computerEmoji.innerHTML = "📋";
-    }else if(computerOption === SCISSORS){
-        computerEmoji.innerHTML = "✂";
-    }
-    
+    emojis(userOption, userEmoji);
+
+    emojis(computerOption, computerEmoji);
+
     switch (result) {
         case TIE:
             resultText.innerHTML = "TIE!";
+            resultText.style.background = '#FAB696';
             break;
         case WIN:
             resultText.innerHTML = "U WIN! :D";
+            resultText.style.background = '#69C181';
             break;
         case LOST:
             resultText.innerHTML = "U LOST :(";
+            resultText.style.background = '#F16767';
             break;
     }
 
+
 }
+
+
+function emojis(play, emoj) {
+    if (play === ROCK) {
+        emoj.innerHTML = "🥌";
+    } else if (play === PAPER) {
+        emoj.innerHTML = "📋";
+    } else if (play === SCISSORS) {
+        emoj.innerHTML = "✂";
+    }
+}
+
+
+
 
 function calcResult(userOption, computerOption) {
     if (userOption === computerOption) {
@@ -70,4 +81,22 @@ function calcResult(userOption, computerOption) {
         if (computerOption === ROCK) return LOST;
         if (computerOption === PAPER) return WIN;
     }
+
+
 }
+
+// while (attempsC < 3 && attempsU < 3) {
+
+//     if (resultText === "U WIN! :D") {
+//         attempsC = attempsC + 1;
+//     } else if (resultText === "U LOST :(") {
+//         attempsU = attempsU + 1;
+//     }
+// }
+
+if (attempsU === 3 || attempsC === 3) {
+    message.innerHTML = "acabó";
+}
+
+
+
